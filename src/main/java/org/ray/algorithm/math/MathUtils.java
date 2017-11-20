7d214e629892e9e0cf0568d7ba309a13cbe21b8b
lanzhou.hlz <lanzhou.hlz@alibaba-inc.com>
2017-11-20 19:47:13 +0800

    add new method.

---
+++ src/main/java/org/ray/algorithm/math/MathUtils.java
@@ -0,0 +1,18 @@
+package org.ray.algorithm.math;
+
+public class MathUtils {
+
+    public static double sqrt(double c) {
+        if (c < 0) {
+            return Double.NaN;
+        }
+
+        double err = 1e-15;
+        double t = c;
+        while (Math.abs(t - c / t) > err * t) {
+            t = (c / t + t) / 2.0;
+        }
+
+        return t;
+    }
+}
