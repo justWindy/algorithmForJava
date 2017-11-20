c9dd541ea983549eaad2faf6cd545a4b207ccd1d
lanzhou.hlz <lanzhou.hlz@alibaba-inc.com>
2017-11-20 20:17:33 +0800

    add new method.

--- src/main/java/org/ray/algorithm/math/MathUtils.java
+++ src/main/java/org/ray/algorithm/math/MathUtils.java
@@ -15,4 +15,47 @@ public class MathUtils {
 
         return t;
     }
+
+    public static int abs(int x) {
+        if (x < 0) {
+            return -x;
+        } else {
+            return x;
+        }
+    }
+
+    public static double abs(double x) {
+        if (x < 0.0) {
+            return -x;
+        } else {
+            return x;
+        }
+    }
+
+    public static boolean isPrime(int N) {
+        if (N < 2) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= N; i++) {
+            if (N % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static double hypotenuse(double a, double b) {
+        return Math.sqrt(a * a + b * b);
+    }
+
+    public static double Harmonic(int N) {
+        double sum = 0.0;
+        for (int i = 1; i <= N; i++) {
+            sum += 1.0 / i;
+        }
+
+        return sum;
+    }
 }
