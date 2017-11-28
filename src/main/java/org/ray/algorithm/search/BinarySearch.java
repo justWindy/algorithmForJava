4773474a7e830689abf87def62cf1af79188f757
lanzhou.hlz <lanzhou.hlz@alibaba-inc.com>
2017-11-28 14:00:11 +0800

    add new class

---
+++ src/main/java/org/ray/algorithm/search/BinarySearch.java
@@ -0,0 +1,40 @@
+package org.ray.algorithm.search;
+
+import org.ray.algorithm.In;
+import org.ray.algorithm.StdIn;
+import org.ray.algorithm.StdOut;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+
+    public static int rank(int key, int[] a) {
+        int low = 0;
+        int high = a.length - 1;
+        while (low <= high) {
+            int middle = (high + low) / 2;
+            if (key < a[middle]) {
+                high = middle - 1;
+            } else if (key > a[middle]) {
+                low = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] whiteList = StdIn.readAllInts();
+        Arrays.sort(whiteList);
+
+        while (!StdIn.isEmpty()) {
+            int key = StdIn.readInt();
+            if (rank(key, whiteList) < 0) {
+                StdOut.print(key);
+            }
+        }
+    }
+
+}
