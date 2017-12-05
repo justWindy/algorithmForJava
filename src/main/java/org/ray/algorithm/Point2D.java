87403150be70e20669767701fa5db5d27e14e171
lanzhou.hlz <lanzhou.hlz@alibaba-inc.com>
2017-12-05 14:06:38 +0800

    add new class

---
+++ src/main/java/org/ray/algorithm/Point2D.java
@@ -0,0 +1,80 @@
+package org.ray.algorithm;
+
+import java.util.Comparator;
+
+public class Point2D implements Comparable<Point2D> {
+
+    public static final Comparator<Point2D> X_ORDER = new XOrder();
+
+    public static final Comparator<Point2D> Y_ORDER = new YOrder();
+
+    public static final Comparator<Point2D> R_ORDER = new ROrder();
+
+    private final double x;
+    private final double y;
+
+    public Point2D(double x, double y) {
+        if (Double.isInfinite(x) || Double.isInfinite(y)) {
+            throw new IllegalArgumentException("Coordinates must be finite");
+        }
+
+        if (Double.isNaN(x) || Double.isNaN(y)) {
+            throw new IllegalArgumentException("Coordinates cannot be NaN");
+        }
+
+        if (x == 0.0) {
+            this.x = 0.0;
+        } else {
+            this.x = x;
+        }
+
+        if (y == 0.0) {
+            this.y = 0.0;
+        } else {
+            this.y = y;
+        }
+    }
+
+    public double x() {
+        return x;
+    }
+
+    public double y() {
+        return y;
+    }
+
+    public double r() {
+        return Math.sqrt(x*x + y*y);
+    }
+
+    @Override
+    public int compareTo(Point2D o) {
+        return 0;
+    }
+
+    private static class XOrder implements Comparator<Point2D> {
+
+        @Override
+        public int compare(Point2D o1, Point2D o2) {
+
+            return 0;
+        }
+    }
+
+    private static class YOrder implements Comparator<Point2D> {
+
+        @Override
+        public int compare(Point2D o1, Point2D o2) {
+            return 0;
+        }
+    }
+
+    private static class ROrder implements Comparator<Point2D> {
+
+        @Override
+        public int compare(Point2D o1, Point2D o2) {
+            return 0;
+        }
+    }
+
+}
