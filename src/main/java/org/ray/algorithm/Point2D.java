bcf0330aab8470b46f024ddcf6837f97ef422828
lanzhou.hlz <lanzhou.hlz@alibaba-inc.com>
2017-12-06 11:18:09 +0800

    add new class

--- src/main/java/org/ray/algorithm/Point2D.java
+++ src/main/java/org/ray/algorithm/Point2D.java
@@ -35,6 +35,33 @@ public class Point2D implements Comparable<Point2D> {
         }
     }
 
+    public static int ccw(Point2D a, Point2D b, Point2D c) {
+        double area2 = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+        if (area2 < 0) {
+            return -1;
+        } else if (area2 > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static double area2(Point2D a, Point2D b, Point2D c) {
+        return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+    }
+
+    public double distanceTo(Point2D that) {
+        double dx = this.x - that.x;
+        double dy = this.y - that.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    public double distanceSquaredTo(Point2D that) {
+        double dx = this.x - that.x;
+        double dy = this.y - that.y;
+        return dx * dx + dy * dy;
+    }
+
     public double x() {
         return x;
     }
@@ -44,12 +71,66 @@ public class Point2D implements Comparable<Point2D> {
     }
 
     public double r() {
-        return Math.sqrt(x*x + y*y);
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public double theta() {
+        return Math.atan2(y, x);
+    }
+
+    private double angleTo(Point2D that) {
+        double dx = that.x - this.x;
+        double dy = that.y - this.y;
+        return Math.atan2(dy, dx);
     }
 
     @Override
     public int compareTo(Point2D o) {
+        if (this.y < o.y) {
+            return -1;
+        }
+
+        if (this.y > o.y) {
+            return 1;
+        }
+
+        if (this.x < o.x) {
+            return -1;
+        }
+
+        if (this.x > o.x) {
+            return 1;
+        }
         return 0;
+    }
+
+    public Comparator<Point2D> polarOrder() {
+        return new PolarOrder();
+    }
+
+    public Comparator<Point2D> atan2Order() {
+        return new Atan2Order();
+    }
+
+    public Comparator<Point2D> distanceToOrder() {
+        return new DistanceToOrder();
+    }
+
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (other == null) {
+            return false;
+        }
+
+        if (other.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Point2D that = (Point2D) other;
+        return this.x == that.x && this.y == that.y;
     }
 
     private static class XOrder implements Comparator<Point2D> {
@@ -76,5 +157,88 @@ public class Point2D implements Comparable<Point2D> {
             return 0;
         }
     }
+
+    private class Atan2Order implements Comparator<Point2D> {
+
+        @Override
+        public int compare(Point2D o1, Point2D o2) {
+            double angle1 = angleTo(o1);
+            double angle2 = angleTo(o2);
+            if (angle1 < angle2) {
+                return -1;
+            } else if (angle1 > angle2) {
+                return 1;
+            } else {
+                return 0;
+            }
+
+        }
+    }
+
+    private class PolarOrder implements Comparator<Point2D> {
+
+        @Override
+        public int compare(Point2D o1, Point2D o2) {
+            double dx1 = o1.x - x;
+            double dy1 = o1.y - y;
+            double dx2 = o2.x - x;
+            double dy2 = o2.y - y;
+
+            if (dy1 >= 0 && dy2 < 0) {
+                return -1;
+            } else if (dy2 >= 0 && dy1 < 0) {
+                return 1;
+            } else if (dy1 == 0 && dy2 == 0) {
+                if (dx1 >= 0 && dx2 < 0) {
+                    return -1;
+                } else if (dx2 >= 0 && dx1 < 0) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            } else {
+                return -ccw(Point2D.this, o1, o2);
+            }
+        }
+    }
+
+    private class DistanceToOrder implements Comparator<Point2D> {
+
+        @Override
+        public int compare(Point2D o1, Point2D o2) {
+            double dist1 = distanceSquaredTo(o1);
+            double dist2 = distanceSquaredTo(o2);
+
+            if (dist1 < dist2) {
+                return -1;
+            } else if (dist1 > dist2) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        int hashX = ((Double) x).hashCode();
+        int hashY = ((Double) y).hashCode();
+
+        return 31 * hashX + hashY;
+    }
+
+    public void draw() {
+        StdDraw.point(x, y);
+    }
+
+    public void drawTo(Point2D that) {
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
 
 }
