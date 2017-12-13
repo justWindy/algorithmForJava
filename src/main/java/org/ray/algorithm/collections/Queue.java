875d1a73799437fed54225846a408e30aa3b7280
lanzhou.hlz <lanzhou.hlz@alibaba-inc.com>
2017-12-13 15:35:25 +0800

    add new class

--- src/main/java/org/ray/algorithm/collections/Queue.java
+++ src/main/java/org/ray/algorithm/collections/Queue.java
@@ -1,6 +1,7 @@
 package org.ray.algorithm.collections;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class Queue<Item> implements Iterable<Item> {
 
@@ -8,14 +9,87 @@ public class Queue<Item> implements Iterable<Item> {
     private Node<Item> last;
     private int        n;
 
+    public Queue() {
+        first = null;
+        last = null;
+        n = 0;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public Item peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue underflow");
+        }
+
+        return first.item;
+    }
+
+    public void enqueue(Item item) {
+        Node<Item> oldLast = last;
+        last = new Node<>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldLast.next = last;
+        }
+        n++;
+    }
+
+    public Item dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue underflow");
+        }
+        Item item = first.item;
+        first = first.next;
+        n--;
+        if (isEmpty()) {
+            last = null;
+        }
+        return item;
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (Item item : this) {
+            builder.append(item);
+            builder.append(' ');
+        }
+
+        return builder.toString();
+    }
+
     private static class Node<Item> {
 
         private Item       item;
         private Node<Item> next;
     }
 
-    @Override
-    public Iterator<Item> iterator() {
-        return null;
+    private class ListIterator<Item> implements Iterator<Item> {
+
+        private Node<Item> current;
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Item next() {
+            return null;
+        }
     }
 }
