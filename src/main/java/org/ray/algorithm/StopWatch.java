99b60aeec84356eb7fc5dd6604c3e8ad8a576a06
ray <helanzhou@gmail.com>
2017-12-31 11:39:24 +0800

    add new class

---
+++ src/main/java/org/ray/algorithm/StopWatch.java
@@ -0,0 +1,5 @@
+package org.ray.algorithm;
+
+public class StopWatch {
+
+}
---
+++ src/main/java/org/ray/algorithm/ThreeSum.java
@@ -0,0 +1,38 @@
+package org.ray.algorithm;
+
+public class ThreeSum {
+
+    private ThreeSum() {
+    }
+
+    public static void printAll(int[] a) {
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; j < n; k++) {
+                    if (a[i] + a[j] + a[k] == 0) {
+                        StdOut.println(a[i] + " " + a[j] + " " + a[k]);
+                    }
+                }
+            }
+        }
+    }
+
+    public static int count(int[] a) {
+        int n = a.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (a[i] + a[j] + a[k] == 0) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+}
