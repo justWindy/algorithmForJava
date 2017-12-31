13416112e28065aa9a65daaa7f299d15f3e7370e
ray <helanzhou@gmail.com>
2017-12-31 18:57:23 +0800

    add new class

---
+++ src/main/java/org/ray/algorithm/DoublingTest.java
@@ -0,0 +1,29 @@
+package org.ray.algorithm;
+
+public class DoublingTest {
+
+    private static final int MAXIMUM_INTEGER = 1000000;
+
+    private DoublingTest() {
+
+    }
+
+    public static double timeTrial(int n) {
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = StdRandom.uniform(-MAXIMUM_INTEGER, MAXIMUM_INTEGER);
+        }
+        StopWatch timer = new StopWatch();
+        ThreeSum.count(a);
+        return timer.elapsedTime();
+    }
+
+    public static void main(String[] args) {
+        for (int n = 250; true; n += n) {
+            double time = timeTrial(n);
+            StdOut.printf("%7d %7.1f\n", n, time);
+        }
+    }
+
+}
