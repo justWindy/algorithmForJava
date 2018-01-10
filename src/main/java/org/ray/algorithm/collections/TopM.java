92cf0fb554a77c693d6acc738bcfbadcce70438e
lanzhou.hlz <lanzhou.hlz@alibaba-inc.com>
2018-01-10 10:21:07 +0800

    add the TopM.java impelementation

--- src/main/java/org/ray/algorithm/collections/TopM.java
+++ src/main/java/org/ray/algorithm/collections/TopM.java
@@ -1,5 +1,9 @@
 package org.ray.algorithm.collections;
 
+import org.ray.algorithm.StdIn;
+import org.ray.algorithm.StdOut;
+import org.ray.algorithm.Transaction;
+
 /**
  * created by ray
  * Date: 10/01/2018
@@ -11,5 +15,30 @@ public class TopM {
 
     }
 
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+
+        MinPQ<Transaction> pq = new MinPQ<>(m + 1);
+
+        while (StdIn.hasNextLine()) {
+            String line = StdIn.readLine();
+
+            Transaction transaction = new Transaction(line);
+            pq.insert(transaction);
+
+            if (pq.size() > m) {
+                pq.delMin();
+            }
+        }
+
+        Stack<Transaction> stack = new Stack<>();
+        for (Transaction transaction : pq) {
+            stack.push(transaction);
+        }
+
+        for (Transaction transaction : stack) {
+            StdOut.println(transaction);
+        }
+    }
 
 }
