8a9104109eeba436bd628d3ffd2e2a9fe05f6a00
ray <helanzhou@gmail.com>
2018-02-10 16:59:27 +0800

    add the BST.java

---
+++ src/main/java/org/ray/algorithm/tree/BST.java
@@ -0,0 +1,475 @@
+package org.ray.algorithm.tree;
+
+import org.ray.algorithm.StdOut;
+import org.ray.algorithm.collections.Queue;
+
+import java.util.NoSuchElementException;
+
+/**
+ * created by ray
+ * Date: 10/02/2018
+ * Time: 15:00
+ */
+public class BST<Key extends Comparable<Key>, Value> {
+
+    private Node root;
+
+    public BST() {
+
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        } else {
+            return x.size;
+        }
+    }
+
+    public boolean contains(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to contains() is null");
+        }
+
+        return get(key) != null;
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        } else {
+            return x.val;
+        }
+    }
+
+    public void put(Key key, Value val) {
+        if (key == null) {
+            throw new IllegalArgumentException("first argument to put() is null");
+        }
+
+        if (val == null) {
+            delete(key);
+            return;
+        }
+        root = put(root, key, val);
+        assert check();
+    }
+
+    private Node put(Node x, Key key, Value val) {
+        if (x == null) {
+            return new Node(key, val, 1);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, val);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, val);
+        } else {
+            x.val = val;
+        }
+        x.size = 1 + size(x.left) + size(x.right);
+        return x;
+    }
+
+    public void deleteMin() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Symbol table underflow");
+        }
+        root = deleteMin(root);
+        assert check();
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) {
+            return x.right;
+        }
+        x.left = deleteMin(x.left);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public void deleteMax() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Symbol table underflow");
+        }
+        root = deleteMax(root);
+        assert check();
+    }
+
+    private Node deleteMax(Node x) {
+        if (x.right == null) {
+            return x.left;
+        }
+        x.right = deleteMax(x.right);
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public void delete(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to delete() is null");
+        }
+        root = delete(root, key);
+        assert check();
+    }
+
+    private Node delete(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = delete(x.left, key);
+        } else if (cmp > 0) {
+            x.right = delete(x.right, key);
+        } else {
+            if (x.right == null) {
+                return x.left;
+            }
+
+            if (x.left == null) {
+                return x.right;
+            }
+
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.size = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public Key min() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("called min() with empty symbol table");
+        }
+
+        return min(root).key;
+    }
+
+    private Node min(Node x) {
+        if (x.left == null) {
+            return x;
+        } else {
+            return min(x.left);
+        }
+    }
+
+    public Key max() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("called max() with empty symbol table");
+        }
+        return max(root).key;
+    }
+
+    private Node max(Node x) {
+        if (x.right == null) {
+            return x;
+        } else {
+            return max(x.right);
+        }
+    }
+
+    public Key floor(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to floor() is null");
+        }
+
+        if (isEmpty()) {
+            throw new NoSuchElementException("called floor() with empty symbol table");
+        }
+
+        Node x = floor(root, key);
+        if (x == null) {
+            return null;
+        } else {
+            return x.key;
+        }
+    }
+
+    private Node floor(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) {
+            return x;
+        }
+        if (cmp < 0) {
+            return floor(x.left, key);
+        }
+
+        Node t = floor(x.right, key);
+        if (t != null) {
+            return t;
+        } else {
+            return x;
+        }
+    }
+
+    public Key ceiling(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to ceiling() is null");
+        }
+        if (isEmpty()) {
+            throw new NoSuchElementException("called ceiling() with empty symbol table");
+        }
+        Node x = ceiling(root, key);
+        if (x == null) {
+            return null;
+        } else {
+            return x.key;
+        }
+    }
+
+    private Node ceiling(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) {
+            return x;
+        }
+        if (cmp < 0) {
+            Node t = ceiling(x.left, key);
+            if (t != null) {
+                return t;
+            } else {
+                return x;
+            }
+        }
+        return ceiling(x.right, key);
+    }
+
+    public Key select(int k) {
+        if (k < 0 || k >= size()) {
+            throw new IllegalArgumentException("called select() with invalid argument: " + k);
+        }
+        Node x = select(root, k);
+        return x.key;
+    }
+
+    private Node select(Node x, int k) {
+        if (x == null) {
+            return null;
+        }
+        int t = size(x.left);
+        if (t > k) {
+            return select(x.left, k);
+        } else if (t < k) {
+            return select(x.right, k - t - 1);
+        } else {
+            return x;
+        }
+    }
+
+    public int rank(Key key) {
+        if (key == null) {
+            throw new IllegalArgumentException("argument to rank() is null");
+        }
+        return rank(key, root);
+    }
+
+    private int rank(Key key, Node x) {
+        if (x == null) {
+            return 0;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return rank(key, x.left);
+        } else if (cmp > 0) {
+            return 1 + size(x.left) + rank(key, x.right);
+        } else {
+            return size(x.left);
+        }
+
+    }
+
+    public Iterable<Key> keys() {
+        return keys(min(), max());
+    }
+
+    public Iterable<Key> keys(Key low, Key high) {
+        if (low == null) {
+            throw new IllegalArgumentException("first argument to keys() is null");
+        }
+
+        if (high == null) {
+            throw new IllegalArgumentException("second argument to keys() is null");
+        }
+
+        Queue<Key> queue = new Queue<>();
+        keys(root, queue, low, high);
+        return queue;
+    }
+
+    private void keys(Node x, Queue<Key> queue, Key low, Key high) {
+        if (x == null) {
+            return;
+        }
+        int cmpLow = low.compareTo(x.key);
+        int cmpHigh = high.compareTo(x.key);
+
+        if (cmpLow < 0) {
+            keys(x.left, queue, low, high);
+        }
+
+        if (cmpLow <= 0 && cmpHigh >= 0) {
+            queue.enqueue(x.key);
+        }
+
+        if (cmpHigh > 0) {
+            keys(x.right, queue, low, high);
+        }
+    }
+
+    public int size(Key low, Key high) {
+        if (low == null) {
+            throw new IllegalArgumentException("first argument to size() is null");
+        }
+
+        if (high == null) {
+            throw new IllegalArgumentException("second argument to size() is null");
+        }
+
+        if (low.compareTo(high) > 0) {
+            return 0;
+        }
+        if (contains(high)) {
+            return rank(high) - rank(low) + 1;
+        } else {
+            return rank(high) - rank(low);
+        }
+    }
+
+    public int height() {
+        return height(root);
+    }
+
+    private int height(Node x) {
+        if (x == null) {
+            return -1;
+        }
+
+        return 1 + Math.max(height(x.left), height(x.right));
+    }
+
+    public Iterable<Key> levelOrder() {
+        Queue<Key> keys = new Queue<>();
+        Queue<Node> queue = new Queue<>();
+
+        queue.enqueue(root);
+        while (!queue.isEmpty()) {
+            Node x = queue.dequeue();
+            if (x == null) {
+                continue;
+            }
+            keys.enqueue(x.key);
+            queue.enqueue(x.left);
+            queue.enqueue(x.right);
+        }
+        return keys;
+    }
+
+    private boolean check() {
+        if (!isBST()) {
+            StdOut.println("Not in symmetric order");
+        }
+
+        if (!isSizeConsistent()) {
+            StdOut.println("Subtree counts not consistent");
+        }
+
+        if (!isRankConsistent()) {
+            StdOut.println("Ranks not consistent");
+        }
+
+        return isBST() && isSizeConsistent() && isRankConsistent();
+    }
+
+    private boolean isBST() {
+        return isBST(root, null, null);
+    }
+
+    private boolean isBST(Node x, Key min, Key max) {
+        if (x == null) {
+            return true;
+        }
+
+        if (min != null && x.key.compareTo(min) <= 0) {
+            return false;
+        }
+
+        if (max != null && x.key.compareTo(max) >= 0) {
+            return false;
+        }
+
+        return isBST(x.left, min, x.key) && isBST(x.right, x.key, max);
+    }
+
+    private boolean isSizeConsistent() {
+        return isSizeConsistent(root);
+    }
+
+    private boolean isSizeConsistent(Node x) {
+        if (x == null) {
+            return true;
+        }
+        if (x.size != size(x.left) + size(x.right) + 1) {
+            return false;
+        }
+        return isSizeConsistent(x.left) && isSizeConsistent(x.right);
+    }
+
+    private boolean isRankConsistent() {
+        for (int i = 0; i < size(); i++) {
+            if (i != rank(select(i))) {
+                return false;
+            }
+        }
+
+        for (Key key : keys()) {
+            if (key.compareTo(select(rank(key))) != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private class Node {
+
+        private Key   key;
+        private Value val;
+        private Node  left, right;
+        private int size;
+
+        public Node(Key key, Value val, int size) {
+            this.key = key;
+            this.val = val;
+            this.size = size;
+        }
+    }
+}
