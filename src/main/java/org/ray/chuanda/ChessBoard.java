2f0bc12da024d7a6c065508568ec589e9023a589
ray <helanzhou@gmail.com>
2018-02-22 17:04:02 +0800

    modified

--- src/main/java/org/ray/chuanda/ChessBoard.java
+++ src/main/java/org/ray/chuanda/ChessBoard.java
@@ -14,7 +14,7 @@ public class ChessBoard {
      * @param tc   棋盘左上角方格的列号
      * @param dr   特殊方格所在的行号
      * @param dc   特殊方格所在的列号
-     * @param size size = 2 ^ k, 棋盘规格为2^k * 2^k
+     * @param size size = 2^k, 棋盘规格为2^k * 2^k
      */
     private void chessBoard(int tr, int tc, int dr, int dc, int size) {
 
