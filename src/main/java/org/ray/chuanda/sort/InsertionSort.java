7e55c3bc8381eb662d6871b73d66b5eb352c580b
ray <helanzhou@gmail.com>
2018-02-24 23:49:18 +0800

    add new class, InsertionSort.java and RecursionSelectSort.java

---
+++ src/main/java/org/ray/chuanda/sort/InsertionSort.java
@@ -0,0 +1,26 @@
+package org.ray.chuanda.sort;
+
+/**
+ * created by ray
+ * Date: 24/02/2018
+ * Time: 23:02
+ */
+public class InsertionSort {
+
+    public static <T extends Comparable<? super T>> void insertionSort(T[] a, int first, int last) {
+        for (int unsorted = first + 1; unsorted < last; unsorted++) {
+            T firstUnsorted = a[unsorted];
+            insertInOrder(firstUnsorted, a, first, unsorted - 1);
+        }
+    }
+
+    private static <T extends Comparable<? super T>> void insertInOrder(T element, T[] a, int begin, int end) {
+        int index = end;
+        while ((index >= begin) && (element.compareTo(a[index]) < 0)) {
+            a[index + 1] = a[index];
+            index--;
+        }
+        a[index + 1] = element;
+    }
+
+}
---
+++ src/main/java/org/ray/chuanda/sort/RecursionSelectSort.java
@@ -0,0 +1,36 @@
+package org.ray.chuanda.sort;
+
+/**
+ * created by ray
+ * Date: 24/02/2018
+ * Time: 23:37
+ */
+public class RecursionSelectSort {
+
+    private static void swap(Object[] a, int i, int j) {
+        Object temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static <T extends Comparable<? super T>> int minValue(T[] a, int start) {
+        int min = start;
+
+        for (int i = 1 + start; i < a.length; i++) {
+            if (a[i].compareTo(a[min]) < 0) {
+                min = i;
+            }
+        }
+
+        return min;
+    }
+
+    public static <T extends Comparable<? super T>> void selectionSort(T[] a, int first, int last) {
+        if (first < last) {
+            int min = minValue(a, first);
+            swap(a, first, min);
+            selectionSort(a, first + 1, last);
+        }
+    }
+
+}
