b2cc581e010aca2c05c1d809d9a62e62e004bd09
ray <helanzhou@gmail.com>
2018-04-21 14:39:35 +0800

    add new class

--- src/main/java/org/ray/leetcode/RemoveDuplicateElement.java
+++ src/main/java/org/ray/leetcode/RemoveDuplicateElement.java
@@ -9,27 +9,35 @@ import java.util.Arrays;
  */
 public class RemoveDuplicateElement {
 
-    public static <T extends Comparable<? super T>> int removeDuplicate(T[] a) {
-        if (a == null || a.length == 0) {
+    public static void main(String[] args) {
+        int[] a = { 1, 1, 2, 2, 2, 2, 3, 4, 4, 4, 4 };
+
+        int[] b = { 1 };
+
+        System.out.println(removeDuplicates(a));
+        System.out.println(Arrays.toString(a));
+
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
         int current = 0;
-        int length = a.length;
+        int index = 1;
+        int cycleNum = nums.length;
 
-        for (int index = current + 1; index < a.length; index++) {
-            if (a[index].compareTo(a[current]) == 0) {
-                length--;
+        for (; index < nums.length && cycleNum >= 0; ) {
+            if (nums[index] == nums[current]) {
+                for (int j = index + 1; j < nums.length; j++) {
+                    nums[j - 1] = nums[j];
+                }
             } else {
                 current = index;
+                index++;
             }
+            cycleNum--;
         }
-        return length;
-    }
-
-    public static void main(String[] args) {
-        Integer[] a = { 1, 1, 2, 2, 2, 2, 3, 4, 4, 4, 4 };
-        System.out.println(
-                "array detail:" + Arrays.toString(a) + "; array length: " + a.length + "; length: " +
-                removeDuplicate(a));
+        return index;
     }
 }
