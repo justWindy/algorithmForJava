ba13c8daeff786fb16136f4948d5850a7a6a0fea
ray <helanzhou@gmail.com>
2018-04-21 16:18:16 +0800

    Palindrome test

---
+++ src/main/java/org/ray/leetcode/AtoiMethod.java
@@ -0,0 +1,65 @@
+package org.ray.leetcode;
+
+/**
+ * created by ray
+ * Date: 2018/4/21
+ * Time: 14:40
+ */
+public class AtoiMethod {
+
+    public static void main(String[] args) {
+        System.out.println(myAtoi("-2147483648"));
+    }
+
+    private static int myAtoi(String str) {
+        String current = str.trim();
+        boolean positiveFlag = false;
+        char[] chars = null;
+        if (current.startsWith("-")) {
+            positiveFlag = true;
+            chars = current.substring(1).toCharArray();
+        } else if (current.startsWith("+")) {
+            chars = current.substring(1).toCharArray();
+        } else {
+            chars = current.toCharArray();
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isDigit(chars[i])) {
+                builder.append(chars[i]);
+            } else {
+                break;
+            }
+        }
+
+        if (builder.length() == 0) {
+            return 0;
+        } else if (builder.length() > String.valueOf(Integer.MAX_VALUE).length()) {
+            if (positiveFlag) {
+                return Integer.MIN_VALUE;
+            } else {
+                return Integer.MAX_VALUE;
+            }
+        }
+
+        long longValue = Long.valueOf(builder.toString());
+        if ((longValue - 1) >= Integer.MAX_VALUE) {
+            if (positiveFlag) {
+                return Integer.MIN_VALUE;
+            }
+        }
+
+        if (longValue > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        if (positiveFlag) {
+            return -(int) longValue;
+        }
+
+        return (int) longValue;
+
+    }
+
+}
---
+++ src/main/java/org/ray/leetcode/PalindromeSolution.java
@@ -0,0 +1,41 @@
+package org.ray.leetcode;
+
+/**
+ * created by ray
+ * Date: 2018/4/21
+ * Time: 16:04
+ */
+public class PalindromeSolution {
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+
+    private static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        } else if (x == 0) {
+            return true;
+        }
+
+        String value = String.valueOf(x);
+
+        char[] chars = value.toCharArray();
+
+        int length = chars.length;
+
+        for (int i = 0; i < length / 2; i++) {
+            char temp = chars[i];
+            chars[i] = chars[length - i - 1];
+            chars[length - i - 1] = temp;
+        }
+
+        if (String.valueOf(x).equals(String.valueOf(chars))) {
+            return true;
+        }
+
+        return false;
+
+    }
+
+}
