d57e17400fc8d368b619c28c0dd6718905cf5af0
ray <helanzhou@gmail.com>
2018-06-17 17:43:45 +0800

    add new class LeetCode6

--- src/main/java/org/ray/leetcode/LeetCode6.java
+++ src/main/java/org/ray/leetcode/LeetCode6.java
@@ -1,5 +1,8 @@
 package org.ray.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * created by ray
  * Date: 2018/5/23
@@ -8,12 +11,52 @@ package org.ray.leetcode;
 public class LeetCode6 {
 
     public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        String s2 = "PAYPALISHIRING";
 
+        System.out.println(convert(s, 3));
+        System.out.println(convert(s2, 4));
     }
 
     private static String convert(String s, int numRows) {
+        if (s == null || s.length() < 1 || numRows <= 1) {
+            return s;
+        }
 
-        return null;
+        List<List<Character>> list = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            list.add(new ArrayList<Character>());
+        }
+
+        int index = 0;
+        int i = 0;
+        int len = s.length();
+
+        while (i < len) {
+            while (index < numRows && i < len) {
+                list.get(index).add(s.charAt(i));
+                i++;
+                index++;
+            }
+
+            if (index == numRows) {
+                index -= 2;
+            }
+
+            while (index > 0 && i < len) {
+                list.get(index).add(s.charAt(i));
+                i++;
+                index--;
+            }
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for (int j = 0; j < numRows; j++) {
+            for (int k=0; k<list.get(j).size(); k++) {
+                builder.append(list.get(j).get(k));
+            }
+        }
+        return builder.toString();
     }
 
 }
